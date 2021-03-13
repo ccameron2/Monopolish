998586af82d790e530a9cb21b67f72698d6dd392
CCameron2 <ccameron2@outlook.com>
2021-03-13 18:09:23 +0000

    Player class, Gameplay loop and GO
    Added player class
    GO square now adds £200 to player
    Players can now play and win/loose the game
    Added Nick's random number generator
    Seed for generator is read in from file

--- Source.cpp
+++ Source.cpp
@@ -5,9 +5,54 @@
 #include <vector>
 #include <memory>
 #include <deque>
+#include <ctime>
 
 using namespace std;
 
+const char POUND = 156;
+
+class CPlayer
+{
+private:
+    string mName;
+    int mMoney;
+    int mPosition;
+public:
+    CPlayer(string name, int money, int position)
+    {
+        mName = name;
+        mMoney = money;
+        mPosition = position;
+    }
+    string GetName()
+    {
+        return mName;
+    }
+    void SetName(string name)
+    {
+        mName = name;
+    }
+    int GetMoney()
+    {
+        return mMoney;
+    }
+    void SetMoney(int money)
+    {
+        mMoney = money;
+    }
+    void ChangeMoney(int changeInMoney)
+    {
+        mMoney += changeInMoney;
+    }
+    int GetPosition()
+    {
+        return mPosition;
+    }
+    void SetPosition(int position)
+    {
+        mPosition = position;
+    }
+};
 
 class CSquare
 {
@@ -111,14 +156,15 @@ public:
     }
 };
 
+int Random();
+int ReadInSeed();
+
 int main()
 {
     vector<CSquare*> sqrList;
     string line = "";
     string word = "";
     ifstream fin("monopoly.txt");
-    
-
     if (fin.is_open())
     {
         while (getline(fin, line,'\n'))
@@ -163,6 +209,7 @@ int main()
                             getline(iss, word, ' ');
                             nameFinal = word;
                             CGo* sqr = new CGo(sqrType, nameFinal, sqrCost, sqrRent, sqrGroup);
+                            sqrList.push_back(sqr);
                             break;
                         }
                         case 3:
@@ -236,14 +283,96 @@ int main()
             }
         }
     }
+    fin.close();
 
-    for (vector<CSquare*>::iterator it = sqrList.begin(); it != sqrList.end(); it++)
+    srand(ReadInSeed());
+
+    //Game loop setup
+    int startMoney = 1500;
+    int startPosition = 0;
+    string nameOne = "Dog";
+    string nameTwo = "Car";
+    vector<CPlayer*> playerList;
+    CPlayer* Dog = new CPlayer(nameOne, startMoney, startPosition);
+    CPlayer* Car = new CPlayer(nameTwo, startMoney, startPosition);
+    playerList.push_back(Dog);
+    playerList.push_back(Car);
+
+    cout << "Welcome to Monopol-ish" << endl;
+
+    //Game Loop
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < playerList.size(); j++)
+        {
+            int roll = Random();
+            cout << playerList[j]->GetName() << " rolls " << roll << endl;
+            if (playerList[j]->GetPosition() + roll > sqrList.size() - 1)
+            {
+                playerList[j]->SetPosition(playerList[j]->GetPosition() + roll - 26);
+            }
+            else
+            {
+                playerList[j]->SetPosition(playerList[j]->GetPosition() + roll);
+            }
+            
+            switch (sqrList[playerList[j]->GetPosition()]->GetType())
+            {
+                case 1:
+                    cout << playerList[j]->GetName() << " lands on " << sqrList[playerList[j]->GetPosition()]->GetName() << endl;
+                    break;
+                case 2:
+                    cout << playerList[j]->GetName() << " passes GO and collects "<< POUND << "200 " << endl;
+                    playerList[j]->ChangeMoney(200);
+                    break;
+                default:
+                    cout << playerList[j]->GetName() << " lands on " << sqrList[playerList[j]->GetPosition()]->GetName() << endl;
+                    break;
+            }           
+
+        }
+    }
+    //End game
+    cout << "Game Over" << endl;
+    for (int i = 0; i < playerList.size(); i++)
+    {
+        cout << playerList[i]->GetName() << " has " << POUND << playerList[i]->GetMoney() << endl;
+    }
+    //Compare final money values
+    if (playerList[1]->GetMoney() > playerList[0]->GetMoney())
+    { 
+        cout << playerList[1]->GetName() << " wins." << endl; 
+    }
+    else
+    { 
+        cout << playerList[0]->GetName() << " wins." << endl; 
+    }
+
+    /*for (vector<CSquare*>::iterator it = sqrList.begin(); it != sqrList.end(); it++)
     {       
             cout << (*it)->GetName(); 
             cout << " " << (*it)->GetCost();
             cout << " " << (*it)->GetRent();
             cout << " " << (*it)->GetGroup() << endl;
-    }
+    }*/
     system("pause");
     return 0;
+}
+
+int Random()//Nick's random number generator.
+{    
+    return static_cast<int>(static_cast<double> (rand()) / (RAND_MAX + 1) * 6.0f + 1);
+}
+
+int ReadInSeed()
+{
+    ifstream fin("seed.txt");
+    int seed = 0;
+
+    if (fin.is_open())
+    {
+        fin >> seed;
+    }
+    fin.close();
+    return seed;
 }
